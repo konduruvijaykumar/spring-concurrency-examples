23337f0187f2abe6b1bb76bf6e03b3d038cd1160
Vijay Konduru <konduruvijaykumar@gmail.com>
2020-05-31 20:33:57 +0530

    initial commit for codes external service

---
+++ external-codes-service/src/main/java/org/pjay/codes/ExternalCodesServiceApplication.java
@@ -0,0 +1,13 @@
+package org.pjay.codes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalCodesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalCodesServiceApplication.class, args);
+	}
+
+}
