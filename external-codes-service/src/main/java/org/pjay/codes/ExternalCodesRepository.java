f7289cdc6b62076b71fb05631e1bd365bf0edee2
Vijay Konduru <konduruvijaykumar@gmail.com>
2020-05-31 21:12:06 +0530

    country repository

---
+++ external-codes-service/src/main/java/org/pjay/codes/ExternalCodesRepository.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package org.pjay.codes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author vijayk
+ *
+ */
+public interface ExternalCodesRepository extends JpaRepository<Integer, Country> {
+
+}
