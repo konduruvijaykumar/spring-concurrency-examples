eea4e91a41e0a59e4387644994c50151fb6aaa9c
Vijay Konduru <konduruvijaykumar@gmail.com>
2020-06-01 16:04:52 +0530

    external service caller spring boot app

---
+++ concurrent-external-service-caller/src/test/java/org/pjay/codes/ConcurrentExternalServiceCallerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pjay.codes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrentExternalServiceCallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
