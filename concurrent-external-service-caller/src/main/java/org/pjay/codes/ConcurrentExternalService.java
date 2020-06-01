d6e186c3775e7b7bd1619bc64814c55acdde2a40
Vijay Konduru <konduruvijaykumar@gmail.com>
2020-06-01 23:48:37 +0530

    service and async call related

--- concurrent-external-service-caller/src/main/java/org/pjay/codes/ConcurrentExternalService.java
+++ concurrent-external-service-caller/src/main/java/org/pjay/codes/ConcurrentExternalService.java
@@ -3,7 +3,14 @@
  */
 package org.pjay.codes;
 
+import java.util.concurrent.CompletableFuture;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author vijayk
@@ -11,5 +18,24 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ConcurrentExternalService {
+
+	@Autowired
+	RestTemplate restTemplate;
+
+	final String EXTERNAL_SERVICE_URL = "http://localhost:8080";
+
+	@Async
+	public CompletableFuture<Country> callExternalCountryService(String iso2countrycode) {
+		String countryURL = EXTERNAL_SERVICE_URL + "/country/" + iso2countrycode;
+		ResponseEntity<Country> responseEntity = restTemplate.exchange(countryURL, HttpMethod.GET, null, Country.class);
+		return CompletableFuture.completedFuture(responseEntity.getBody());
+	}
+
+	@Async
+	public CompletableFuture<Phone> callExternalPhoneService(String iso2countrycode) {
+		String phoneURL = EXTERNAL_SERVICE_URL + "/phone/" + iso2countrycode;
+		ResponseEntity<Phone> responseEntity = restTemplate.exchange(phoneURL, HttpMethod.GET, null, Phone.class);
+		return CompletableFuture.completedFuture(responseEntity.getBody());
+	}
 
 }
